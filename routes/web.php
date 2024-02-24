304cf5307dbd5237c3ebd341f3f4acfc1a5bb2ad
natghi2010 <natnael@mine-marketing.com>
2024-02-24 18:07:56 +0300

    Update web

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
-
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::post('/login', [AuthController::class, 'login'])->name('loginUser');
